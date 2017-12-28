0f3d81175cbc438b18d2d3386f49d5757f92c9cb
Pavan Kumar Thikkavarapu <pavankumart@CESCHN.COM>
2017-12-28 16:01:53 +0530

    modified context

--- ces-cor2/src/app/content/timeline/timeline.component.ts
+++ ces-cor2/src/app/content/timeline/timeline.component.ts
@@ -1,14 +1,16 @@
+// import { Component, ElementRef } from '@angular/core';
 import { Component } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { TimelineService } from './../../common/services/timeline.service';
 import { NgFor } from '@angular/common';
 import { WindowRefService } from '../../common/services/timeline.service';
-declare var jquery:any;
+// declare var jquery:any;
 declare var $ :any;
 @Component({
   // selector: 'header-bar',
   templateUrl: './timeline.component.html',
   styleUrls: ['./timeline.component.css'],
+  host: {'(window:scroll)': 'track($event)'},
   animations: [
      trigger('fade',[
        transition('void  => *',[
@@ -22,10 +24,12 @@ declare var $ :any;
 })
 export class TimelineComponent {
   
-  posts
+  posts;
+  goUpIcon;
 
   private _window: Window;
 
+  // constructor(private timelinedata: TimelineService, windowRef: WindowRefService, private elRef: ElementRef) {
   constructor(private timelinedata: TimelineService, windowRef: WindowRefService) {
     // this.posts = timelinedata.getPosts()
     console.log("component START");
@@ -34,18 +38,20 @@ export class TimelineComponent {
     this.posts = timelinedata.getNewPosts()
     console.log("component DONE", this.posts);
   }
+  
+  track($event) {
+    if(window.pageYOffset > 100){
+     
+      this.goUpIcon = 'display-block';
+    } else {
+      
+      this.goUpIcon = 'display-none';
+    }
+  }
 
   doThing() {
     console.log(this._window);
-    this._window.scroll(0,0);
+    this._window.scroll({left:0,top:0, behavior:'smooth'});
   }
 
-
-
-
-
-  title = 'abgular 4 with jquery';
-  toggleTitle(){
-    $('.title').slideToggle();
-  }
 }
